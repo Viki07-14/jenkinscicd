a38e0dc84f60388b24342aa97a75f0f8d145c4a2
viki <vickymano1411@gmail.com>
2025-10-29 19:30:03 +0000

    my last commit

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "hi bro webhook is working well !!!Hello, Jenkins build triggered successfully and the successfull mail also sent!!!"
+echo "hi bro webhook is working well !!!mkvdkndvnjnjdvnvnHello, Jenkins build triggered successfully and the successfull mail also sent!!!"
